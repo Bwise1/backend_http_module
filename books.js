aa78f378f1fa09ff14d0622a0cf7ea77a87d993f
Bwise1 <oguntoyebenjamin2@gmail.com>
2024-02-10 13:33:36 +0200

    Added DELETE to api

--- books.js
+++ books.js
@@ -1,48 +1,68 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 let books = [];
 var data = fs.readFileSync("books.json", "utf8");
 books = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/books") {
-    //GET METHOD
-    if (req.method === "GET") {
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(JSON.stringify(books));
-      // res.end();
-    }
+  console.warn(req.url);
+  // if (req.url === "/books") {
+  //GET METHOD
 
-    //POST METHOD
-    else if (req.method === "POST") {
-      let body = "";
-
-      req.on("data", (chunk) => {
-        body += chunk;
-      });
-
-      req.on("end", () => {
-        books.push(JSON.parse(body));
-        fs.writeFileSync("books.json", JSON.stringify(books));
-        res.writeHead(201, { "Content-Type": "application/json" });
-        res.end(
-          JSON.stringify({
-            message: "Successfully added Book",
-            data: JSON.parse(body),
-          })
-        );
-      });
-
-      //   res.end();
-    } else {
-      res.writeHead(405, { "Content-Type": "text/plain" });
-      res.end("Method not allowed");
-    }
-  } else {
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("Page not found");
+  if (req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(books));
+    // res.end();
   }
+
+  //POST METHOD
+  else if (req.method === "POST") {
+    let body = "";
+
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+
+    req.on("end", () => {
+      books.push(JSON.parse(body));
+      fs.writeFileSync("books.json", JSON.stringify(books, null, 2));
+      res.writeHead(201, { "Content-Type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "Successfully added Book",
+          data: JSON.parse(body),
+        })
+      );
+    });
+
+    //   res.end();
+  } else if (req.method === "DELETE") {
+    const parsedUrl = url.parse(req.url, true);
+    const id = parsedUrl.query.id;
+
+    for (let i in books) {
+      if (books[i].id === id) {
+        console.log("Found");
+        books.splice(i, 1);
+      }
+      fs.writeFileSync("books.json", JSON.stringify(books, null, 2));
+    }
+    res.writeHead(204);
+    // console.log(books);
+    res.end();
+  }
+  // Write your PUT logic
+  else {
+    res.writeHead(405, { "Content-Type": "text/plain" });
+    res.end("Method not allowed");
+  }
+  // }
+  // else {
+  //   res.writeHead(404, { "Content-Type": "text/plain" });
+  //   res.end("Page not found");
+  // }
 });
 
 const port = 5001;
