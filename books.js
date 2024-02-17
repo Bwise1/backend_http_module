4bb808b9a827c3c69fb99b64d17f57743c881a37
Bwise1 <oguntoyebenjamin2@gmail.com>
2024-02-17 12:17:57 +0200

    Add PUT request

--- books.js
+++ books.js
@@ -10,7 +10,6 @@ const server = http.createServer((req, res) => {
   console.warn(req.url);
   // if (req.url === "/books") {
   //GET METHOD
-
   if (req.method === "GET") {
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(JSON.stringify(books));
@@ -54,7 +53,44 @@ const server = http.createServer((req, res) => {
     res.end();
   }
   // Write your PUT logic
-  else {
+  else if (req.method === "PUT") {
+    const parsedUrl = url.parse(req.url, true);
+    const id = parsedUrl.query.id;
+    console.log(id);
+    let body = "";
+    let found = false;
+
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+
+    req.on("end", () => {
+      // books.push(JSON.parse(body));
+
+      for (let i in books) {
+        if (books[i].id === id) {
+          found = true;
+          books[i] = JSON.parse(body);
+          fs.writeFileSync("books.json", JSON.stringify(books, null, 2));
+          res.writeHead(200, { "Content-Type": "application/json" });
+          res.end(
+            JSON.stringify({
+              message: "Successfully Edited Book",
+              data: JSON.parse(body),
+            })
+          );
+          break;
+        }
+      }
+
+      if (!found) {
+        console.log("book not found");
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("Book with ID not found");
+      }
+      // console.log(books);
+    });
+  } else {
     res.writeHead(405, { "Content-Type": "text/plain" });
     res.end("Method not allowed");
   }
