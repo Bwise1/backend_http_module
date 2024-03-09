9a143fc55e4912d96f109c69ebbe6257bf7e5fd0
Bwise1 <oguntoyebenjamin2@gmail.com>
2024-03-09 12:57:46 +0200

    add, subtract, delete, multiply and history

--- Class_006/logic.js
+++ Class_006/logic.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 function message() {
   return "Server Running swiftly from module";
 }
@@ -6,5 +8,41 @@ function add(num1, num2) {
   return num1 + num2;
 }
 
-module.exports.message = message;
-module.exports.add = add;
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function readFile() {
+  let history = [];
+  var data = fs.readFileSync("history.json", "utf8");
+  history = JSON.parse(data);
+
+  return history;
+}
+
+function writeFile(file) {
+  history = readFile();
+  history.push(file);
+  fs.writeFileSync("history.json", JSON.stringify(history, null, 2));
+}
+
+// module.exports.message = message;
+// module.exports.add = add;
+
+module.exports = {
+  message,
+  add,
+  subtract,
+  multiply,
+  divide,
+  readFile,
+  writeFile,
+};
--- Class_006/server.js
+++ Class_006/server.js
@@ -1,19 +1,76 @@
 const express = require("express");
-const { message, add } = require("./logic");
+const bodyParser = require("body-parser");
+const {
+  message,
+  add,
+  subtract,
+  multiply,
+  divide,
+  readFile,
+  writeFile,
+} = require("./logic");
 
 const app = express();
-
+app.use(bodyParser.json());
 const PORT = 5001;
 
 app.get("/", (req, res) => {
   res.send(message());
 });
 
-app.get("/add", (req, res) => {
-  let result = add(3, 4);
-  console.log(result);
-  //   result + "";
+app.post("/add", (req, res) => {
+  // let result = add(3, 4);
+
+  const { num1, num2 } = req.body;
+  let result = add(num1, num2);
+  let file = {
+    operation: "add",
+    time: new Date(),
+    operands: {
+      num1: num1,
+      num2: num2,
+    },
+    result: result,
+  };
+  writeFile(file);
   res.status(200).send({ result: result });
+});
+
+app.post("/subtract", (req, res) => {
+  const { num1, num2 } = req.body;
+
+  let result = subtract(num1, num2);
+  let file = {
+    operation: "subtract",
+    time: new Date(),
+    operands: {
+      num1: num1,
+      num2: num2,
+    },
+    result: result,
+  };
+  writeFile(file);
+
+  res.status(200).send({ result: result });
+});
+
+app.post("/multiply", (req, res) => {
+  const { num1, num2 } = req.body;
+
+  let result = multiply(num1, num2);
+  res.status(200).send({ result: result });
+});
+
+app.post("/divide", (req, res) => {
+  const { num1, num2 } = req.body;
+
+  let result = divide(num1, num2);
+  res.status(200).send({ result: result });
+});
+
+app.get("/history", (req, res) => {
+  let history = readFile();
+  res.status(200).send({ data: history });
 });
 
 app.listen(PORT, () => {
